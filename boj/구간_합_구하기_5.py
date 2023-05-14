f24dfd3ec360245aec8ef47427b2d2729f44d056
std-yong <std-yong@github.com>
2023-05-15 02:57:51 +0900

    23/05/15

--- boj/구간_합_구하기_5.py
+++ boj/구간_합_구하기_5.py
@@ -1,19 +1,22 @@
-# 입력을 받습니다.
 n, m = map(int, input().split())
-matrix = [list(map(int, input().split())) for _ in range(n)]
+vv = [list(map(int, input().split())) for _ in range(n)] #nxn 2차원 배열 생성 및 대입
 
-# 부분합을 계산합니다.
-sum_matrix = [[0] * (n+1) for _ in range(n+1)]  # 부분합 행렬을 저장할 리스트
+
+psum = [[0 for _ in range(n+1)] for _ in range(n+1)]
 for i in range(1, n+1):
     for j in range(1, n+1):
-        sum_matrix[i][j] = matrix[i-1][j-1] + sum_matrix[i-1][j] + sum_matrix[i][j-1] - sum_matrix[i-1][j-1]
+        psum[i][j] = vv[i-1][j-1] + (psum[i-1][j]) + (psum[i][j-1]) - (psum[i-1][j-1])
 
-temp = []
-# 쿼리에 대한 결과를 출력합니다.
-for _ in range(m):
+def Part_Sum_2Arr(x1, y1, x2, y2, psum): #좌표에 대한 구간 합 구하는 공식
+    S = psum[x2+1][y2+1] - psum[x1][y2+1] - psum[x2+1][y1] + psum[x1][y1]
+    return S
+
+result = []
+
+for m in range(m):
     x1, y1, x2, y2 = map(int, input().split())
-    result = sum_matrix[x2][y2] - sum_matrix[x2][y1-1] - sum_matrix[x1-1][y2] + sum_matrix[x1-1][y1-1]
-    temp.append(result)
-    
-for _ in range(m):
-    print(temp[i])
+    temp = Part_Sum_2Arr(x1-1,y1-1,x2-1,y2-1, psum)
+    result.append(temp)
+
+for i in range(len(result)):
+    print(result[i])
