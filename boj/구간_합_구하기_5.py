e2d3208be310d620b6cc11b8dbf71182b307e8f9
std-yong <std-yong@github.com>
2023-05-15 16:55:05 +0900

    23/05/15_16:55

--- boj/구간_합_구하기_5.py
+++ boj/구간_합_구하기_5.py
@@ -1,6 +1,7 @@
-n, m = map(int, input().split())
-vv = [list(map(int, input().split())) for _ in range(n)] #nxn 2차원 배열 생성 및 대입
+import sys
 
+n, m = map(int, sys.stdin.readline().split())
+vv = [list(map(int, sys.stdin.readline().split())) for _ in range(n)] #nxn 2차원 배열 생성 및 대입
 
 psum = [[0 for _ in range(n+1)] for _ in range(n+1)]
 for i in range(1, n+1):
@@ -14,7 +15,7 @@ def Part_Sum_2Arr(x1, y1, x2, y2, psum): #좌표에 대한 구간 합 구하는 
 result = []
 
 for m in range(m):
-    x1, y1, x2, y2 = map(int, input().split())
+    x1, y1, x2, y2 = map(int, sys.stdin.readline().split())
     temp = Part_Sum_2Arr(x1-1,y1-1,x2-1,y2-1, psum)
     result.append(temp)
 
