7340754e50108de389912a2823ed9e4c03829ad6
std-yong <std-yong@github.com>
2023-06-13 23:05:17 +0900

    23/06/13_23:05

---
+++ boj/단어_정렬.py
@@ -0,0 +1,15 @@
+import sys
+input = sys.stdin.readline
+
+N = int(input())
+check = []
+
+for i in range(N):
+    check.append(input().rstrip())
+
+check = list(set(check))
+# set 자료형으로 중복을 제거 후 list로 만들어 정렬한다
+check.sort(key = len)
+
+for i in check:
+    print(i)    
